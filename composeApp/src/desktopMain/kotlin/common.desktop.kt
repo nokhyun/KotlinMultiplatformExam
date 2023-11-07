b2165f935817b57eb3e029c636391b8012236345
kwangjun <kwangjun.cho@ikoob.com>
2023-11-07 16:36:12 +0900

    font

--- composeApp/src/desktopMain/kotlin/common.desktop.kt
+++ composeApp/src/desktopMain/kotlin/common.desktop.kt
@@ -1,1 +1,9 @@
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+
 actual fun getPlatformName(): String = "Desktop"
+
+@Composable
+actual fun font(name: String, res: String, weight: FontWeight, style: FontStyle): Font = androidx.compose.ui.text.platform.Font("font/$res.ttf", weight, style)
